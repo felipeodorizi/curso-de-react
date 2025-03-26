08e9b759a9d7ca736f2dd197c73d5bacbc565877
framework-CPTEC <rameworkcptec@gmail.com>
2025-03-26 13:54:32 -0300

    Alterando estilo TaskPage

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -9,7 +9,7 @@ function TaskPage() {
     const title = searchParams.get("title");
     const description = searchParams.get("description");
     return (
-    <div className="h-screen w-screen bg-slate-500 p-6">
+    <div className="h-screen w-screen mx-auto bg-slate-500 p-6">
         <div className="flex"> 
             <button onClick={() => navigate(-1)} className="text-slate-100"> <ChevronLeftIcon /> </button>
             <h1 className="text-3xl text-slate-100 font-bold text-center">Detalhes da Tarefa</h1>
